a4985c57b9ed2d41ea3a0f84013576e2565d7f79
gawronskijakub <44299334+gawronskijakub@users.noreply.github.com>
2019-11-23 15:58:42 +0100

    visually changes
    added navigation and logo in header, added social icons (anchors) in footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,10 +1,17 @@
 <footer class="footer">
   <p class="footer__copyright">
+    Made with &nbsp;<i class="fa fa-heart"></i>&nbsp; by JG / 
     <?php print date("Y"); ?>
   </p>
   <p class="footer__socials">
-    <i class="fab fa-facebook-square"></i>
-    <i class="fab fa-linkedin"></i>
-    <i class="fab fa-github-square"></i>
+    <a href="https://www.facebook.com/kubek201" class="footer__link link">
+      <i class="fab fa-facebook-square"></i>
+    </a>
+    <a href="https://www.linkedin.com/in/jakub-gawro%C5%84ski-51a14a18b/" class="footer__link link">
+      <i class="fab fa-linkedin"></i>
+    </a>
+    <a href="https://github.com/gawronskijakub" class="footer__link link">
+      <i class="fab fa-github-square"></i>
+    </a>
   </p>
 </footer>
--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,23 @@
 <header class="header">
   <h1 class="header__entitle">
-    <a href="/let/index.php" class="header__link link">
-      <!-- add image and navigation -->
+    <a href="/let/index.php" class="header__link link" title="LET Strona główna">
+      <img src="/let/images/logo.png" alt="LET logo" class="header__logo">
     </a>
   </h1>
+  <nav class="nav">
+    <ul class="nav__list">
+      <li class="nav__item">
+        <a href="/let/index.php" class="nav__link link">Strona główna</a>
+      </li>
+      <li class="nav__item">
+        <a href="/let/login.php" class="nav__link link">Zaloguj się</a>
+      </li>
+      <li class="nav__item">
+        <a href="/let/about.php" class="nav__link link">O mnie</a>
+      </li>
+      <li class="nav__item">
+        <a href="/let/contact.php" class="nav__link link">Kontakt</a>
+      </li>
+    </ul>
+  </nav>
 </header>
