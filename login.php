87803f6162a9e1a21a9f1c63298b2972b5a922d0
gawronskijakub <44299334+gawronskijakub@users.noreply.github.com>
2019-11-24 10:44:09 +0100

    design and behavior changes to content
    added basic login/registration form, simple submit file to check button funcionality and visually updated content

--- login.php
+++ login.php
@@ -9,23 +9,36 @@
     <?php include("templates/header.php"); ?>
     <main class="main">
       <article class="article">
-        <section class="article__section">
-          <form action="" method="post" class="article__form">
-              <h2 class="article__form__header">Zaloguj się: </h2>
-              <label for="firstName">Imię: </label>
-              <input type="text" name="firstName" tabindex="1">
-          </form>
-          <div class="article__buttons">
-            <button class="article__button">Logowanie</button>
-            <button class="article__button">Rejestracja</button>
+        <section class="article__section" id="log">
+          <div class="form__container">
+            <form action="submit.php" method="post" class="article__form" id="login_form">
+                <h2 class="article__form__header">Zaloguj się: </h2>
+                <label for="firstName">Imię: </label>
+                <input type="text" name="firstName" tabindex="1">
+            </form>
+            <div class="article__buttons">
+              <button class="article__button" form="login_form">Logowanie&nbsp;<i class="fa fa-arrow-right"></i></button>
+              <button class="article__button" type="button"><a href="#reg" class="article__link link">Rejestracja&nbsp;<i class="fa fa-arrow-down"></i></a></button>
+            </div>
           </div>
         </section>
-        <section class="article__section">
-          d
+        <section class="article__section" id="reg">
+        <div class="form__container">
+            <form action="" method="post" class="article__form" id="reg_form">
+                <h2 class="article__form__header">Zarejestruj się: </h2>
+                <label for="firstName">Imię: </label>
+                <input type="text" name="firstName" tabindex="1">
+            </form>
+            <div class="article__buttons">
+              <button class="article__button" type="button"><a href="#log" class="article__link link">Logowanie&nbsp;<i class="fa fa-arrow-up"></i></a></button>
+              <button class="article__button" form="reg_form">Rejestracja&nbsp;<i class="fa fa-arrow-right"></i></button>
+            </div>
+          </div>
         </section>
       </article>
     </main>
     <?php include("templates/footer.php"); ?>
+    <script src="/let/scripts/script.js"></script>
   </div>
 </body>
 </html>
