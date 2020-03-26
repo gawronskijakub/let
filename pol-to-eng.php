a927f64d57034daaf541a8755ee238a4ff631803
James <44299334+gawronskijakub@users.noreply.github.com>
2020-03-26 15:48:31 +0100

    first course
    finished Polish to English self check course

--- pol-to-eng.php
+++ pol-to-eng.php
@@ -26,7 +26,10 @@
         <section class="article__section">
           <section class="courses">
             <section class="courses__course">
-              <h2 class="course__entitle">Nauka słówek</h2>
+              <h2 class="course__entitle">
+                Nauka słówek <br> 
+                <p></p>
+              </h2>
               <div class="course__content">
                 <div class="arrows">
                   <a href="#" class="control_next">></a>
@@ -34,11 +37,13 @@
                 </div>
                 <div id="slider1">
                   <?php
-                    $sql = "SELECT `polish` FROM `words`;";
+                    $sql = "SELECT `polish`, `english` FROM `words`;";
                     $res = mysqli_query($link, $sql);
+                    $j = 1;
                     echo "<ul class='list'>";
                     while($row = mysqli_fetch_assoc($res)) {
-                      echo "<li class='polish'><img src='/let/images/poland.jpg'>$row[polish]</li>";
+                      echo "<li class='polish'><img src='/let/images/poland.jpg'>$row[polish] <span class='$j'>$row[english]</span></li>";
+                      $j++;
                     }
                     echo "</ul>";
                     ?>
@@ -47,9 +52,11 @@
                   <?php
                     $sql = "SELECT `english` FROM `words`;";
                     $res = mysqli_query($link, $sql);
+                    $i = 1;
                     echo "<form class='list'>";
                     while($row = mysqli_fetch_assoc($res)) {
-                      echo "<input type='text'>";
+                      echo "<input type='text' class='$i'>";
+                      $i++;
                     }
                     echo "</form>";
                   ?>
@@ -66,28 +73,25 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script>
     var slideCount = $('#slider1 ul li').length;
-    // var inputCount = $('#slider2 form input').length;
-
     var slideWidth = $('#slider1 ul li').width();
-
     var slideHeight = $('#slider1 ul li').height();
 
     const moveLeft = () => {
         $('#slider1 ul').animate({
             left: + slideWidth
-        }, 200, () => {
+        }, 750, () => {
             $('#slider1 ul li:last-child').prependTo('#slider1 ul');
             $('#slider1 ul').css('left', '');
         });
         $('#slider2 ul').animate({
             left: + slideWidth
-        }, 200, () => {
+        }, 750, () => {
             $('#slider2 ul li:last-child').prependTo('#slider2 ul');
             $('#slider2 ul').css('left', '');
         });
         $('#slider2 form').animate({
             left: + slideWidth
-        }, 200, () => {
+        }, 750, () => {
             $('#slider2 form input:last-child').prependTo('#slider2 form');
             $('#slider2 form').css('left', '');
         });
@@ -96,19 +100,19 @@
     const moveRight = () => {
         $('#slider1 ul').animate({
             left: - slideWidth
-        }, 200, () => {
+        }, 750, () => {
             $('#slider1 ul li:first-child').appendTo('#slider1 ul');
             $('#slider1 ul').css('left', '');
         });
         $('#slider2 ul').animate({
             left: - slideWidth
-        }, 200, () => {
+        }, 750, () => {
             $('#slider2 ul li:first-child').appendTo('#slider2 ul');
             $('#slider2 ul').css('left', '');
         });
         $('#slider2 form').animate({
             left: - slideWidth
-        }, 200, () => {
+        }, 750, () => {
             $('#slider2 form input:first-child').appendTo('#slider2 form');
             $('#slider2 form').css('left', '');
         });
@@ -120,6 +124,34 @@
 
     $('a.control_next').on("click", () => {
         moveRight();
+    });
+
+    var spans = $('#slider1 ul li span');
+    var inputs = $('#slider2 form input');
+    var points = 0;
+    var pointsPlace = document.querySelector(".course__entitle p");
+    pointsPlace.innerHTML = `Liczba punktów (max ${slideCount}): 0`;
+    
+    function checkWord(e) {
+      if(e.key === "Enter") {
+       [...spans].forEach(span => {
+        if(span.classList.contains(this.classList)) {
+          if(this.value === span.textContent) {
+            this.style["background-color"] = "rgba(34, 153, 84, 0.5)";
+            this.disabled = "disabled";
+            points++;
+          } else {
+            this.style["background-color"] = "rgba(203, 67, 53, 0.5)";
+          }
+        }
+       });
+       pointsPlace.innerHTML = `Liczba punktów (max ${slideCount}): ${points}`;
+       console.log(points);
+      }
+    }
+
+    [...inputs].forEach(input =>{
+      input.addEventListener("keypress", checkWord);
     });
   </script>
 </body>
