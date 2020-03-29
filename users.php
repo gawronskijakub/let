964db41d9f5a8200cbaaaeed3d53f38fe959b76d
James <44299334+gawronskijakub@users.noreply.github.com>
2020-03-29 19:13:07 +0200

    users displaying
    added user displaying for admin to maintain users settings

--- users.php
+++ users.php
@@ -1,5 +1,6 @@
 <?php 
   include("./templates/config.php"); 
+  include("./scripts/connect.php"); 
   session_start();  
 
   if(empty($_SESSION['user'])) {
@@ -25,16 +26,42 @@
     <main class="main">
       <article class="article">
         <section class="article__section">
-        <section class="tiles">
-            <section class="tiles__tile">Lorem ipsum dolor.</section>
-            <section class="tiles__tile">Lorem ipsum dolor.</section>
-            <section class="tiles__tile">Lorem ipsum dolor.</section>
-            <section class="tiles__tile">Lorem ipsum dolor.</section>
+          <section class="users">
+            <?php
+              $users = "SELECT * FROM `users`;";
+              $res = mysqli_query($link, $users);
+              echo "<table>";
+              echo "<tr>
+                <th>ID</th>
+                <th>Imię</th>
+                <th>Nazwisko</th>
+                <th>Login</th>
+                <th>E-mail</th>
+                <th>Status konta</th>
+                <th>Typ konta</th>
+              </tr>";
+              while ($row = mysqli_fetch_assoc($res)) {
+                echo "<tr>
+                  <td>$row[user_id]</td>
+                  <td>$row[first_name]</td>
+                  <td>$row[last_name]</td>
+                  <td>$row[login]</td>
+                  <td>$row[email]</td>
+                  <td>$row[account_status]</td>
+                  <td>$row[account_type]</td>
+              </tr>";
+              }
+
+              echo "</table>";
+            ?>
           </section>
         </section>
       </article>
     </main>
-    <?php include("./templates/footer.php"); ?>
+    <?php 
+      include("./templates/footer.php"); 
+      mysqli_close($link);
+    ?>
   </div>
   <script src="./scripts/script.js"></script>
 </body>
