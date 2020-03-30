af704964c80bb24180e417087dd894387b91657c
James <44299334+gawronskijakub@users.noreply.github.com>
2020-03-30 22:15:15 +0200

    favicon
    added (working as it wants to..) favicon

--- templates/head-tag.php
+++ templates/head-tag.php
@@ -4,4 +4,5 @@
 <title><?php print $pageTitle; ?></title>
 <link href="https://fonts.googleapis.com/css?family=Titillium+Web&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.1.0/css/all.css">
-<link rel="stylesheet" href="styles/style.css">
+<link rel="stylesheet" href="./styles/style.css">
+<link rel="shortcut icon" href="./../images/logo.ico">
